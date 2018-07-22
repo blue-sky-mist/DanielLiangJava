fcad1b7279489cf65cfada8ae945027caca233a7
blue-sky-mist <kishore.yugal@yahoo.co.in>
2018-07-22 23:36:34 +0530

    Initial commit

---
+++ src/daniel/liang/chapter2/ComputeArea21.java
@@ -0,0 +1,9 @@
+package daniel.liang.chapter2;
+
+public class ComputeArea21 {
+    public static void  main(String [] args) {
+        double circleRadius = 10;
+        double circleArea = circleRadius*circleRadius*3.14;
+        System.out.println("The area of the circle with radius " + circleRadius +  "is :" + circleArea);
+    }
+}
