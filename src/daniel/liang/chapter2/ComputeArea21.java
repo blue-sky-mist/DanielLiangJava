7600f31b79a03f7a8720ec3b9c801abf8b58b47d
blue-sky-mist <kishore.yugal@yahoo.co.in>
2018-07-22 23:54:33 +0530

    minor formatting

--- src/daniel/liang/chapter2/ComputeArea21.java
+++ src/daniel/liang/chapter2/ComputeArea21.java
@@ -4,6 +4,6 @@ public class ComputeArea21 {
     public static void  main(String [] args) {
         double circleRadius = 10;
         double circleArea = circleRadius*circleRadius*3.14;
-        System.out.println("The area of the circle with radius " + circleRadius +  "is :" + circleArea);
+        System.out.println("The area of the circle with radius " + circleRadius +  " is :" + circleArea);
     }
 }
